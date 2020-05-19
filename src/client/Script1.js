6b827c6b13aa812951aeac63118028001b58d43c
Aleksey Ostroukhov <pm2t29jf47@gmail.com>
2020-05-19 14:21:10 +0300

    add simple client

--- src/client/Script1.js
+++ src/client/Script1.js
@@ -1,11 +1,25 @@
-window.onload = load;
 
-function load() {
-    alert("load event detected!");
-    let url = 'https://localhost:44383/api/TableOfProcesses/GetStatistics';
+
+async function loadStatistics() {    
+    let url = 'http://localhost:50253/api/tableofprocesses/statistics';
     let response = await fetch(url);
+    let statistics = await response.json();    
+    let nonpagedSystemMemorySizeHeader = document.getElementById('nonpaged-system-memory-size');
+    let pagedMemorySizeInBytesHeader = document.getElementById('paged-memory-size');    
+    nonpagedSystemMemorySizeHeader.innerText = statistics.sumNonpagedSystemMemorySize64InBytes;
+    pagedMemorySizeInBytesHeader.innerText = statistics.sumPagedMemorySize64InBytes;
 
-    let commits = await response.json(); // читаем ответ в формате JSON
-
-    alert(commits[0].author.login);
+    let tableOfProcessesBody = document.getElementById('table-of-processes-body');
+    populateTableBody(tableOfProcessesBody, statistics.processes);
 }
+
+function populateTableBody(tableBody, data) {
+    let html = '';
+    for (let element of data) {
+        html += `<tr><td>${element.pid}</td><td>${element.command}</td><td>${element.nonpagedSystemMemorySize64InBytes}</td><td>${element.pagedMemorySize64InBytes}</td></tr>`;
+    }
+    tableBody.innerHTML = html;
+}
+
+loadStatistics();
+let timer = setInterval(loadStatistics, 500);
